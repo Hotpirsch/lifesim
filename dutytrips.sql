0d1bb03860e915cafa828d5594573f9dba934b69
Hotpirsch <git@christoph-krueger.de>
2021-10-21 12:59:36 +0200

    duty trips added

---
+++ dutytrips.sql
@@ -0,0 +1,27 @@
+CREATE TABLE mytable(
+   ID          INTEGER  NOT NULL PRIMARY KEY 
+  ,Destination VARCHAR(11) NOT NULL
+);
+INSERT INTO mytable(ID,Destination) VALUES (1,'Amsterdam');
+INSERT INTO mytable(ID,Destination) VALUES (2,'Berlin');
+INSERT INTO mytable(ID,Destination) VALUES (3,'Brussels');
+INSERT INTO mytable(ID,Destination) VALUES (4,'Budapest');
+INSERT INTO mytable(ID,Destination) VALUES (5,'Dresden');
+INSERT INTO mytable(ID,Destination) VALUES (6,'Gdansk');
+INSERT INTO mytable(ID,Destination) VALUES (7,'Gothenburg');
+INSERT INTO mytable(ID,Destination) VALUES (8,'Hamburg');
+INSERT INTO mytable(ID,Destination) VALUES (9,'Helsinki');
+INSERT INTO mytable(ID,Destination) VALUES (10,'Hongkong');
+INSERT INTO mytable(ID,Destination) VALUES (11,'Honolulu');
+INSERT INTO mytable(ID,Destination) VALUES (12,'Kigali');
+INSERT INTO mytable(ID,Destination) VALUES (13,'Leipzig');
+INSERT INTO mytable(ID,Destination) VALUES (14,'Minneapolis');
+INSERT INTO mytable(ID,Destination) VALUES (15,'Munich');
+INSERT INTO mytable(ID,Destination) VALUES (16,'Paris');
+INSERT INTO mytable(ID,Destination) VALUES (17,'Seattle');
+INSERT INTO mytable(ID,Destination) VALUES (18,'Shenzen');
+INSERT INTO mytable(ID,Destination) VALUES (19,'Singapore');
+INSERT INTO mytable(ID,Destination) VALUES (20,'Stuttgart');
+INSERT INTO mytable(ID,Destination) VALUES (21,'Toronto');
+INSERT INTO mytable(ID,Destination) VALUES (22,'Vevey');
+INSERT INTO mytable(ID,Destination) VALUES (23,'Zagreb');
